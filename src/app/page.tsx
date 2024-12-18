a606ad02b320ad55be2625c76d440b452f49db2a
Pramod Singh Gusain <spramodgusain@gmail.com>
2024-12-18 11:43:11 +0530

    chore(scss): added other files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-center text-primary mx-4xl ">Next React Application</h1>
+      <div className="container">
+        <h1 className="text-center text-primary mx-4xl">Next React Application</h1>
+      </div>
     </main>
   );
 }
